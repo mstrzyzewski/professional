1d92931af9763dab26910a62e6fa565dc4600482
Maksymilian Strzyżewski <m.strzyzewski@macopedia.com>
2024-07-02 21:20:54 +0200

    [Chapter 1.4] Create Config model

---
+++ app/code/SwiftOtter/OrderExport/Model/Config.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SwiftOtter\OrderExport\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class Config
+{
+    const CONFIG_PATH_ENABLED = 'sales/order_export/enabled';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private ScopeConfigInterface $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Get is enabled value
+     *
+     * @param string $scopeType
+     * @param string|null $scopeCode
+     * @return bool
+     */
+    public function isEnabled(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_ENABLED, $scopeType, $scopeCode);
+    }
+}
